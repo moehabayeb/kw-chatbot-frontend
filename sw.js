7338420148f0ac67e639d6d1977236f4d6731166
moehabayeb <mohammadhabayeb2001@gmail.com>
2025-06-10 06:41:34 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// A simple service worker to make the site installable
-self.addEventListener('fetch', function(event) {
-  // We are not adding any offline logic for this app, 
-  // as it requires a live connection to the backend.
-  // We simply pass the request through.
-  event.respondWith(fetch(event.request));
-});
